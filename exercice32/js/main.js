fba2c0f6114e78afb54d64f26a5d0a6d9b1e7a43
fusbudget <andrewsanser@gmail.com>
2022-01-18 21:38:48 +0100

    add solutions with fetch in ex 32,33

--- exercice32/js/main.js
+++ exercice32/js/main.js
@@ -1,10 +1,9 @@
-$(() => {
-    $('.btn').click(function() {
-        $.ajax({
-            url: "https://restcountries.com/v3.1/name/france",
-            success: function(data, status, res) {
-                console.log(data[0].name.common, data[0].capital[0]);
-            }
-        });
-    });
+document.addEventListener("DOMContentLoaded", function() {
+    document.querySelector('.btn').addEventListener('click', getCountry);
 });
+
+async function getCountry() {
+    let country = await fetch("https://restcountries.com/v3.1/name/france");
+    country = await country.json();
+    console.log(country[0].name.common, country[0].capital[0]);
+}
--- exercice33/js/main.js
+++ exercice33/js/main.js
@@ -1,12 +1,12 @@
-$(() => {
-    $('.btn').click(function() {
-        $.ajax({
-            url: "https://restcountries.com/v3.1/name/france",
-            success: function(data, status, res) {
-                // console.log(data[0].name.common, data[0].capital[0]);
-                $('#country').html(data[0].name.common);
-                $('#capital').html(data[0].capital[0]);
-            }
-        });
-    });
+document.addEventListener("DOMContentLoaded", function() {
+    document.querySelector('.btn').addEventListener('click', getCountry);
 });
+
+async function getCountry() {
+    let country = await fetch("https://restcountries.com/v3.1/name/france");
+    country = await country.json();
+    document.querySelector('#country')
+        .insertAdjacentHTML('afterbegin', `${country[0].name.common}`);
+    document.querySelector('#capital')
+        .insertAdjacentHTML('afterbegin', `${country[0].capital[0]}`);
+}
