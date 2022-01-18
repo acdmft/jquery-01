d9f36a5c14cc3c10ec9e361dbd03948b40eadd48
fusbudget <andrewsanser@gmail.com>
2022-01-18 13:43:57 +0100

    add solution to exercise29 (main.js)

---
+++ exercice29/js/main.js
@@ -0,0 +1,16 @@
+$(() => {
+    $('.btn').click(changeInputValidity);
+    $('.form-control').focusout(changeInputValidity);
+});
+
+function changeInputValidity () {
+    let textInput = $('.form-control').val();
+    console.log(textInput.length);
+    if (textInput.length > 5) {
+        $('.form-control').removeClass('is-invalid');
+        $('.form-control').addClass('is-valid');
+    } else {
+        $('.form-control').removeClass('is-valid');
+        $('.form-control').addClass('is-invalid');
+    }
+}
